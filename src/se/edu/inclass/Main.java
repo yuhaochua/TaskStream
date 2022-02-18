00158df5ef4883cb4773cf526b2adac9fefe228e
yuhaochua <yuhaochua99@gmail.com>
2022-02-18 17:24:21 +0800

    Use print data with streams method

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -19,6 +19,7 @@ public class Main {
         printDeadlines(tasksData);
 
         System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
+        printDataWithStreams(tasksData);
 
     }
 
